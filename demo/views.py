faf90f8f4e10cf16b7cb3a4471dd3365931ccc24
Alexey Popkov <alvlpv969@gmail.com>
2025-03-26 17:19:06 +1000

    demo2

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,11 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('Здесь будет сайт')
+
+def time(request):
+    return HttpResponse(f'Time = {datetime.datetime.now().time()}')
--- lesson1/urls.py
+++ lesson1/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from demo.views import index
+from demo.views import index, time
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('time/', time),
 ]
